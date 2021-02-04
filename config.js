676a5c33ab9e8a02e6e2d59247b32b2c5ce88a70
Marcelo Pereira <marcelops.developer@gmail.com>
2021-02-04 01:35:19 -0300

    removendo PROXY_URL

--- config.js
+++ config.js
@@ -6,15 +6,13 @@ require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 const config = {
   client_id: process.env.CLIENT_ID,
   redirect_uri: process.env.REDIRECT_URI,
-  client_secret: process.env.CLIENT_SECRET,
-  proxy_url: process.env.PROXY_URL
+  client_secret: process.env.CLIENT_SECRET
 };
 
 const envVarsSchema = Joi.object({
   client_id: Joi.string().required(),
   redirect_uri: Joi.string().required(),
-  client_secret: Joi.string().required(),
-  proxy_url: Joi.string().required()
+  client_secret: Joi.string().required()
 });
 
 const { error } = envVarsSchema.validate(config);
