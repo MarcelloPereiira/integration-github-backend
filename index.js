b22083dfd12ddf1277b4f625a1909f4ec72bd8a0
Marcelo Pereira <marcelops.developer@gmail.com>
2021-02-04 03:46:36 -0300

    Adicionando swagger

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const FormData = require("form-data");
 const fetch = require("node-fetch");
 const cors = require("cors");
 const { client_id, redirect_uri, client_secret } = require("./config");
+const swaggerFile = require('./swagger_output.json')
+const swaggerUi = require('swagger-ui-express')
 
 const app = express();
 
 app.use(cors());
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "text/*" }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +23,15 @@ app.use((req, res, next) => {
 
 app.post("/authenticate", (req, res) => {
   const { code } = req.body;
-
+  
+  /* #swagger.parameters['code'] = {
+          in: 'body',
+          description: 'Code for authentication.',
+          required: true,
+          schema: {
+            code: 'string'
+          }
+  } */
   const data = new FormData();
   data.append("client_id", client_id);
   data.append("client_secret", client_secret);
@@ -55,6 +66,14 @@ app.post("/authenticate", (req, res) => {
 
 app.get("/users", (req, res) => {
   const { since, per_page} = req.query;
+  /* #swagger.parameters['since'] = {
+          description: 'A user ID. Only return users with an ID greater than this ID..',
+          type: 'string'
+  } */
+  /* #swagger.parameters['per_page'] = {
+          description: 'Results per page (max 100).',
+          type: 'string'
+  } */
   return fetch(`https://api.github.com/users?since=${since}&per_page=${per_page}`, {
     method: "GET",
   })
@@ -65,7 +84,32 @@ app.get("/users", (req, res) => {
   .catch((error) => {
     return res.status(400).json(error);
   });;
-})
+});
+
+app.get(`/user/:id/repos`, (req, res) => {
+  const { page, per_page} = req.query;
+  const { id } = req.params;
+
+  /* #swagger.parameters['page'] = {
+          description: 'Page number of the results to fetch.',
+          type: 'string'
+  } */
+  /* #swagger.parameters['per_page'] = {
+          description: 'Results per page (max 100).',
+          type: 'string'
+  } */
+  
+  return fetch(`https://api.github.com/user/${id}/repos?page=${page}&per_page=${per_page}`, {
+    method: "GET",
+  })
+  .then((response) => response.json())
+  .then((response) => {
+    return res.status(200).json(response);
+  })
+  .catch((error) => {
+    return res.status(400).json(error);
+  });;
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
